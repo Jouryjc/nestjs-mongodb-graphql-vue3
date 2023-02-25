5ad71113f943198a07aad4443c5a72346a60c977
Jouryjc <494208562@qq.com>
2023-02-25 11:08:33 +0800

    feat: update mongo cmd and fix configmap

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,3 @@
-
-
 // export const MONGO_HOST = process.env.MONGO_HOST ?? 'localhost'
 // export const MONGO_PORT = process.env.MONGO_PORT ?? '27017';
 // export const MONGO_DB = process.env.MONGO_DB ?? 'test';
